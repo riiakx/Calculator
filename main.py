f44b397b2706245793f370f58d42cf9713a8b60e
riiakx <faaria4@hotmail.co.uk>
2023-02-09 20:13:52 +0000

    Creates a menu for the user to select from and takes in user input

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("Welcome to my Calculator")
+print("1) Addition (+)")
+print("2) Subtraction (-)")
+print("3) Division (/)")
+print("4) Multiplication (*)")
+
+operation = input("Enter an operation: ")
