b93c23a5300b1a7930741582e061fd4250793915
riiakx <faaria4@hotmail.co.uk>
2023-02-09 20:27:06 +0000

    Adds missing white space

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ print("4) Multiplication (*)")
 
 operation = input("Enter an operation: ")
 
-num1 = int(input("Enter the first number:"))
-num2 = int(input("Enter the second number:"))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 if operation == "+":
     print(num1 + num2)
