fc22b49d2e2772739d7a97ef504a70a9d795f336
riiakx <faaria4@hotmail.co.uk>
2023-02-09 20:17:46 +0000

    Creates two variables to take in two numbers

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ print("3) Division (/)")
 print("4) Multiplication (*)")
 
 operation = input("Enter an operation: ")
+
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
